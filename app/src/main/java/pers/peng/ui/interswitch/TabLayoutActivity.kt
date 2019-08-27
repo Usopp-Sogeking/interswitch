0db1de5746c11faff76cb77c9187468ef406bd90
Peng <3501230085@qq.com>
2019-08-27 14:38:18 +0800

    界面切换的实现方式

--- app/src/main/java/pers/peng/ui/interswitch/TabLayoutActivity.kt
+++ app/src/main/java/pers/peng/ui/interswitch/TabLayoutActivity.kt
@@ -2,20 +2,54 @@ package pers.peng.ui.interswitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 
 class TabLayoutActivity : AppCompatActivity() {
+    private lateinit var mDrawerLayout: DrawerLayout
     private lateinit var mViewPager: ViewPager
     private lateinit var mTabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tab_layout)
+        val toolbar=findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val ab=supportActionBar
+        ab?.setHomeAsUpIndicator(R.drawable.ic_menu)
+        ab?.setDisplayHomeAsUpEnabled(true)
         mViewPager=findViewById(R.id.viewpager)
         mTabLayout=findViewById(R.id.tabs)
+        mDrawerLayout=findViewById(R.id.dl_main_drawer)
+        val navigationView: NavigationView =findViewById(R.id.nv_main_navigation)
+        if(navigationView!=null){
+            navigationView.setNavigationItemSelectedListener {
+                it.isChecked = true
+                when(it.itemId){
+                    R.id.nav_first->mViewPager.currentItem=0
+                    R.id.nav_second->mViewPager.currentItem=1
+                    R.id.nav_three->mViewPager.currentItem=2
+                }
+                mDrawerLayout.closeDrawers()
+                true
+            }
+        }
         initViewPager()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                return true}
+        }
+        return super.onOptionsItemSelected(item)
     }
     private fun initViewPager(){
         val titles:MutableList<String> =ArrayList()
